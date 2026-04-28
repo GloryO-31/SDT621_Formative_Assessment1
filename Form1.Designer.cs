4eb0af1964d2981336c2dedb73c23bdc86fb48d5
GloryO-31 <20240991@Ctucareer.co.za>
2026-04-28 20:05:39 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Programming_Languages_Question_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBoxLanguages = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.RoyalBlue;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(35, 376);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 35);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Add Language";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(179, 376);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 35);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Remove";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBoxLanguages
+            // 
+            this.listBoxLanguages.FormattingEnabled = true;
+            this.listBoxLanguages.ItemHeight = 16;
+            this.listBoxLanguages.Location = new System.Drawing.Point(38, 89);
+            this.listBoxLanguages.Name = "listBoxLanguages";
+            this.listBoxLanguages.Size = new System.Drawing.Size(569, 228);
+            this.listBoxLanguages.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.MediumVioletRed;
+            this.label1.Location = new System.Drawing.Point(132, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(461, 29);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "My Favourite Programming Languages";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(35, 335);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(514, 22);
+            this.txtInput.TabIndex = 7;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(35, 425);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 16);
+            this.lblStatus.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Pink;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBoxLanguages);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Favourite Programming Languages";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBoxLanguages;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Programming_Languages_Question_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Adding functionalities to the add and remove buttons
+            string language = txtInput.Text.Trim();
+
+            // 1. Prevent empty input
+            if (string.IsNullOrEmpty(language))
+            {
+                MessageBox.Show("Please enter a language.");
+                return;
+            }
+
+            // 2. Preventing duplicates
+            if (listBoxLanguages.Items.Contains(language))
+            {
+                MessageBox.Show("This language is already in your favorites!");
+            }
+            else
+            {
+                listBoxLanguages.Items.Add(language);
+                txtInput.Clear();
+            }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBoxLanguages.SelectedItem != null)
+            {
+                string removedLang = listBoxLanguages.SelectedItem.ToString();
+                listBoxLanguages.Items.Remove(removedLang);
+
+                // 3. Display the date and time of removal
+                lblStatus.Text = $"Removed {removedLang} at {DateTime.Now:dd MMM yyyy HH:mm:ss}";
+            }
+            else
+            {
+                MessageBox.Show("Please select a language to remove.");
+            }
+        }
+
+        private void listBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
